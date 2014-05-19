2922d7c82c6baf03d2e3cb4e699b923a15a92585
Matt Niemeir <matt.niemeir@gmail.com>
2014-05-19 05:11:07 +0000

    Tweak ASSOCF implementation

--- utils.lisp
+++ utils.lisp
@@ -6,20 +6,20 @@
 
 (defmacro assocf (key val place &rest keyargs &environment env)
   "Sets first instance of key in alist to val, adding entry if necessary."
-  (multiple-value-bind (vars vals nv set access)
+  (multiple-value-bind (vars vals bind set access)
       (get-setf-expansion place env)
     (alexandria:with-gensyms (gkey gval cons gaccess)
       `(let ((,gkey ,key)
              (,gval ,val))
          (let* (,@(mapcar #'list vars vals)
-                (,gaccess ,access)
-                (,(car nv)
-                 (let ((,cons (assoc ,gkey ,gaccess ,@keyargs)))
-                   (if ,cons
-                       (prog1 ,gaccess
-                         (setf (cdr ,cons) ,gval))
-                       (acons ,gkey ,gval ,gaccess)))))
-           ,set)))))
+                (,gaccess ,access))
+           (multiple-value-bind ,bind
+               (let ((,cons (assoc ,gkey ,gaccess ,@keyargs)))
+                 (if ,cons
+                     (prog1 ,gaccess
+                       (setf (cdr ,cons) ,gval))
+                     (acons ,gkey ,gval ,gaccess)))
+             ,set))))))
 
 (defmacro ensure-dynamic-bindings ((&rest symbols) &body body)
   "Ensure symbols are dynamically bound without shadowing extant bindings."
